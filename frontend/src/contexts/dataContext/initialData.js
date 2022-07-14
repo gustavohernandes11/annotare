cfc49b10a8aaf980af7155abf736bdcde03fc9c2
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-07-14 11:04:48 -0300

    mudanças na mensagem inicial

--- frontend/src/contexts/dataContext/initialData.js
+++ frontend/src/contexts/dataContext/initialData.js
@@ -3,16 +3,18 @@ export const initialData = {
         {
             id: 1,
             name: "Notas do desenvolvedor",
-            color: "green",
+            color: "crimson",
         },
     ],
     annotations: [
         {
             id: 1,
             heading: "Obrigado por testar meu projeto.",
-            content: `Nesse projeto você pode salvar, ler e editar anotações organizadas por categoria.
-            \n Além disso é possível alterar o tema (que ficará salvo para o próximo acesso), e o layout de apresentação das anotações.
-             \n Para maiores detalhes, você pode conferir o código no Github, basta acessar pelo link no final da página.`,
+            content: `Nesse projeto você pode salvar, ler e editar anotações organizadas por categorias.
+            \n Além disso é possível alterar o tema e o layout de apresentação das anotações.
+            \n Para maiores detalhes, você pode conferir o código no Github, basta acessar pelo link no final da página.
+            \n 
+            \n - Gu`,
             category: "Notas do desenvolvedor",
             created_at: "",
         },
