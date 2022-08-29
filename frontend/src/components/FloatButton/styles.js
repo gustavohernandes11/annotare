d974a8ece9aae7a7e58439da50ea92c1944db5bd
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-08-28 21:08:52 -0300

    style: botão de adicionar anotações movido de lugar.

--- frontend/src/components/FloatButton/styles.js
+++ frontend/src/components/FloatButton/styles.js
@@ -9,13 +9,25 @@ export const Button = styled(Container)`
         justify-content: center;
         width: 50px;
         height: 50px;
-        border-radius: 10px;
+        border-radius: 5px;
         bottom: 20px;
         right: 20px;
         background: ${theme.colors.secondary};
         display: none;
         -webkit-box-shadow: 0px 5px 15px -3px rgba(0, 0, 0, 0.51);
         box-shadow: 0px 5px 15px -3px rgba(0, 0, 0, 0.51);
-        color: ${theme.name === 'light' ? theme.colors.white : 'inherit'};
+        color: ${theme.name === "light" ? theme.colors.white : "inherit"};
+
+        @media screen and (max-width: ${theme.screens.mobileL}) {
+            bottom: 3rem;
+            right: -3px;
+            border-top-left-radius: 50%;
+            border-bottom-left-radius: 50%;
+            /* right: 50%;
+            transform: translateX(50%); */
+
+            width: 35px;
+            height: 35px;
+        }
     `}
 `;
